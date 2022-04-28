dde088956b97104632e77188a60c7419d9e1ae33
wildanym <wildanym@gmail.com>
2022-04-28 13:32:42 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryRed: "ED3237",
+        primaryGrey: "848688",
+      },
+    },
+  },
+  plugins: [],
+};
