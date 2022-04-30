de698f7227c45972dfba949e8054c1a564721d76
wildanym <wildanym@gmail.com>
2022-04-30 08:13:57 +0700

    edit title web

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "sahaware-assesment",
+    title: "sahaware-assessment",
     htmlAttrs: {
       lang: "en",
     },
